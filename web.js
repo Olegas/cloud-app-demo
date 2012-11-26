7e283b52e412631b203180700c2645479ed12911
Oleg Elifantiev <oleg@elifantiev.ru>
2012-11-26 15:29:35 +0400

    uploading and showing

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var db = "mongodb://heroku:fa7056b1616dc231badddde339ad2dc3@alex.mongohq.com:10042/app9488681";
+var db = "mongodb://test:test@alex.mongohq.com:10042/app9488681";
 var bucket = "static.imageshare.elifantiev.ru";
 
 var request = require("request");
@@ -7,6 +7,19 @@ var express = require('express');
 var connect = require('connect');
 var knox = require('knox');
 var path = require('path');
+var mongoose = require('mongoose');
+
+mongoose.connect(db);
+
+var Schema = mongoose.Schema;
+
+var ImagesSchema = new Schema({
+   number: Number,
+   realname: String
+});
+
+var ImageItem = mongoose.model('image', ImagesSchema);
+
 
 var client = knox.createClient({
    key: 'AKIAI6RJ6XPKER5IWNIQ'
@@ -32,16 +45,38 @@ app.get('/', function(req, res){
    });
 });
 
+app.get('/i/:num', function(req, res){
+   ImageItem.find({ number: req.params.num }, function(err, docs){
+      if(err)
+         res.send(500, err);
+      else {
+         res.redirect('http://' + bucket + "/" + docs[0].realname);
+      }
+   })
+});
+
 app.post("/putfile", function(req, res){
    if(res) {
       if(req.files && req.files.pic) {
          client.putFile(req.files.pic.path, req.files.pic.name, { 'x-amz-acl': 'public-read' }, function(e, r){
             if(e)
-               res.send(500);
+               res.send(500, e);
             else {
-               // TODO save to database
-               // print shortlink
-               res.send(204);
+               var i = new ImageItem();
+               i.number = +new Date();
+               i.realname = req.files.pic.name;
+               i.save(function(e){
+                  if(e) {
+                     res.send(500, e);
+                  } else {
+                     fs.readFile(path.join(__dirname, 'views', 'imageready.html'), function(e, r){
+                        if(e)
+                           res.send(500, e);
+                        else
+                           res.send(r.toString().replace(/%NUM%/g, i.number));
+                     });
+                  }
+               });
             }
          });
       } else {
